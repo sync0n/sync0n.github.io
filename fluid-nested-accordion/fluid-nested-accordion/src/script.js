4085c72725796313b1b07355a9699002100dbacc
sync0n <43514738+sync0n@users.noreply.github.com>
2019-06-25 01:40:29 +0300

    up

--- fluid-nested-accordion/fluid-nested-accordion/src/script.js
+++
@@ -1,5 +0,0 @@
-$('.nested-accordion').find('.comment').slideUp();
-$('.nested-accordion').find('h3').click(function(){
-  $(this).next('.comment').slideToggle(100);
-  $(this).toggleClass('selected');
-});
